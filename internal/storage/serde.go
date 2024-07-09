CLiqing/milvus

commit 21b547035a922296e0346a5d872248ec88cc5bbe

fix: Allocate new slice for each batch in streaming reader (#33359)
Related to #33268

Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

internal/storage/serde.go
@@ -588,9 +588,8 @@ func (deser *DeserializeReader[T]) Next() error {
 		deser.pos = 0
 		deser.rec = deser.rr.Record()
 
-		if deser.values == nil {
-			deser.values = make([]T, deser.rec.Len())
-		}
+		// allocate new slice preventing overwrite previous batch
+		deser.values = make([]T, deser.rec.Len())
 		if err := deser.deserializer(deser.rec, deser.values); err != nil {
 			return err
 		}

commit 3c7e17628e522a0006e4e9afe3ce04768645d664

fix: reuse deserialize result to help improve memory management (#34507)
Fixed #33268
The original reuse is broken by #33359

Signed-off-by: Ted Xu <ted.xu@zilliz.com>

internal/storage/serde.go
@@ -538,8 +538,9 @@ func (deser *DeserializeReader[T]) Next() error {
 		deser.pos = 0
 		deser.rec = deser.rr.Record()
 
-		// allocate new slice preventing overwrite previous batch
-		deser.values = make([]T, deser.rec.Len())
+		if deser.values == nil || len(deser.values) != deser.rec.Len() {
+			deser.values = make([]T, deser.rec.Len())
+		}
 		if err := deser.deserializer(deser.rec, deser.values); err != nil {
 			return err
 		}