CLiqing/milvus

commit 5d2abf738c3d158e813768fc192450e7b152e348

feat: impl StructArray -- support element-level query (#47906)
issue: https://github.com/milvus-io/milvus/issues/42148
design doc:
https://github.com/milvus-io/milvus-design-docs/blob/main/design_docs/20260306-struct.md

---------

Signed-off-by: SpadeA <tangchenjie1210@gmail.com>

internal/core/src/query/ExecPlanNodeVisitor.cpp
@@ -299,7 +299,8 @@ ExecPlanNodeVisitor::visit(RetrievePlanNode& node) {
 
     // Do task execution
     auto result = ExecuteTask(plan, query_context);
-    setupRetrieveResult(result, op_context, node, retrieve_result, segment);
+    setupRetrieveResult(
+        result, op_context, node, retrieve_result, segment, query_context);
 }
 
 void
@@ -308,7 +309,8 @@ ExecPlanNodeVisitor::setupRetrieveResult(
     const OpContext& op_context,
     const RetrievePlanNode& node,
     RetrieveResult& tmp_retrieve_result,
-    const segcore::SegmentInternalInterface* segment) {
+    const segcore::SegmentInternalInterface* segment,
+    std::shared_ptr<milvus::exec::QueryContext> query_context) {
     if (result == nullptr) {
         // Return empty field_data arrays with correct schema (0 rows, N columns)
         // to ensure result structure matches the expected output type.
@@ -335,11 +337,25 @@ ExecPlanNodeVisitor::setupRetrieveResult(
                "children inside row vector must be of column vector for now");
     tmp_retrieve_result.total_data_cnt_ = first_column->size();
     if (first_column->IsBitmap()) {
-        tracer::AutoSpan _("Find Limit Pk", tracer::GetRootSpan());
         BitsetTypeView view(first_column->GetRawData(), first_column->size());
-        auto results_pair = segment->find_first(node.limit_, view);
-        tmp_retrieve_result.result_offsets_ = std::move(results_pair.first);
-        tmp_retrieve_result.has_more_result = results_pair.second;
+        if (query_context->element_level_query()) {
+            // Element-level query: bitset is element-level, need to convert to (doc_id, element_index)
+            tmp_retrieve_result.element_level_ = true;
+            tracer::AutoSpan _(
+                "Element Level Find", tracer::GetRootSpan(), true);
+            auto array_offsets = query_context->get_array_offsets();
+            auto [doc_offsets, element_indices, has_more] =
+                segment->find_first_n_element(
+                    node.limit_, view, array_offsets.get());
+            tmp_retrieve_result.result_offsets_ = std::move(doc_offsets);
+            tmp_retrieve_result.element_indices_ = std::move(element_indices);
+            tmp_retrieve_result.has_more_result = has_more;
+        } else {
+            tracer::AutoSpan _("Find Limit Pk", tracer::GetRootSpan());
+            auto results_pair = segment->find_first_n(node.limit_, view);
+            tmp_retrieve_result.result_offsets_ = std::move(results_pair.first);
+            tmp_retrieve_result.has_more_result = results_pair.second;
+        }
         retrieve_result_opt_ = std::move(tmp_retrieve_result);
     } else {
         // load data in the result vector into retrieve_result

client/go.mod
@@ -7,16 +7,13 @@ require (
 	github.com/cockroachdb/errors v1.9.1
 	github.com/google/uuid v1.6.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
-	github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260202035249-b2c299650f7e
+	github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260304062516-e7e54d966ef2
 	github.com/milvus-io/milvus/pkg/v2 v2.6.4-0.20251104142533-a2ce70d25256
-	github.com/minio/minio-go/v7 v7.0.73
 	github.com/quasilyte/go-ruleguard/dsl v0.3.23
 	github.com/samber/lo v1.27.0
 	github.com/stretchr/testify v1.11.1
 	github.com/tidwall/gjson v1.17.1
-	go.opentelemetry.io/otel v1.40.0
 	go.uber.org/atomic v1.11.0
-	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
 	google.golang.org/grpc v1.71.0
 	google.golang.org/protobuf v1.36.5
 )
@@ -37,11 +34,9 @@ require (
 	github.com/form3tech-oss/jwt-go v3.2.3+incompatible // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/getsentry/sentry-go v0.12.0 // indirect
-	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/godbus/dbus/v5 v5.0.4 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
@@ -53,11 +48,9 @@ require (
 	github.com/jonboulle/clockwork v0.2.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
-	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
@@ -71,7 +64,6 @@ require (
 	github.com/prometheus/common v0.55.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
-	github.com/rs/xid v1.5.0 // indirect
 	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
@@ -100,6 +92,7 @@ require (
 	go.etcd.io/etcd/server/v3 v3.5.5 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.59.0 // indirect
+	go.opentelemetry.io/otel v1.40.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.20.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.20.0 // indirect
 	go.opentelemetry.io/otel/metric v1.40.0 // indirect
@@ -110,6 +103,7 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect

client/go.sum
@@ -135,8 +135,6 @@ github.com/go-check/check v0.0.0-20180628173108-788fd7840127/go.mod h1:9ES+weclK
 github.com/go-errors/errors v1.0.1 h1:LUHzmkK3GUKUrL/1gfBUxAHzcev3apQlezX/+O7ma6w=
 github.com/go-errors/errors v1.0.1/go.mod h1:f4zRHt4oKfwPJE5k8C9vpYG+aDHdBFUsgrm6/TyX73Q=
 github.com/go-gl/glfw v0.0.0-20190409004039-e6da0acd62b1/go.mod h1:vR7hzQXu2zJy9AVAgeJqvqgH9Q5CA+iKCZ2gyEVpxRU=
-github.com/go-ini/ini v1.67.0 h1:z6ZrTEZqSWOTyH2FlglNbNgARyHG8oLW9gMELqKr06A=
-github.com/go-ini/ini v1.67.0/go.mod h1:ByCAeIL28uOIIG0E3PJtZPDL8WnHpFKFOtgjp+3Ies8=
 github.com/go-kit/kit v0.8.0/go.mod h1:xBxKIO96dXMWWy0MnWVtmwkA9/13aqxPnvrjFYMA2as=
 github.com/go-kit/kit v0.9.0/go.mod h1:xBxKIO96dXMWWy0MnWVtmwkA9/13aqxPnvrjFYMA2as=
 github.com/go-kit/log v0.1.0/go.mod h1:zbhenjAZHb184qTLMA9ZjW7ThYL0H2mk7Q6pNt4vbaY=
@@ -155,8 +153,6 @@ github.com/go-stack/stack v1.8.0/go.mod h1:v0f6uXyyMGvRgIKkXu+yp6POWl0qKG85gN/me
 github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee/go.mod h1:L0fX3K22YWvt/FAX9NnzrNzcI4wNYi9Yku4O0LKYflo=
 github.com/gobwas/pool v0.2.0/go.mod h1:q8bcK0KcYlCgd9e7WYLm9LpyS+YeLd8JVDW6WezmKEw=
 github.com/gobwas/ws v1.0.2/go.mod h1:szmBTxLgaFppYjEmNtny/v3w89xOydFnnZMcgRRu/EM=
-github.com/goccy/go-json v0.10.3 h1:KZ5WoDbxAIgm2HNbYckL0se1fHD6rz5j4ywS6ebzDqA=
-github.com/goccy/go-json v0.10.3/go.mod h1:oq7eo15ShAhp70Anwd5lgX2pLfOS3QCiwU/PULtXL6M=
 github.com/godbus/dbus/v5 v5.0.4 h1:9349emZab16e7zQvpmsbtjc18ykshndd8y2PG3sgJbA=
 github.com/godbus/dbus/v5 v5.0.4/go.mod h1:xhWf0FNVPg57R7Z0UbKHbJfkEywrmjJnf7w5xrFpKfA=
 github.com/gogo/googleapis v0.0.0-20180223154316-0cd9801be74a/go.mod h1:gf4bu3Q80BeJ6H1S1vYPm8/ELATdvryBaNFGgqEef3s=
@@ -301,9 +297,6 @@ github.com/klauspost/compress v1.9.7/go.mod h1:RyIbtBH6LamlWaDj8nUwkbUhJ87Yi3uG0
 github.com/klauspost/compress v1.17.9 h1:6KIumPrER1LHsvBVuDa0r5xaG0Es51mhhB9BQB2qeMA=
 github.com/klauspost/compress v1.17.9/go.mod h1:Di0epgTjJY877eYKx5yC51cX2A2Vl2ibi7bDH9ttBbw=
 github.com/klauspost/cpuid v1.2.1/go.mod h1:Pj4uuM528wm8OyEC2QMXAi2YiTZ96dNQPGgoMS4s3ek=
-github.com/klauspost/cpuid/v2 v2.0.1/go.mod h1:FInQzS24/EEf25PyTYn52gqo7WaD8xa0213Md/qVLRg=
-github.com/klauspost/cpuid/v2 v2.2.8 h1:+StwCXwm9PdpiEkPyzBXIy+M9KUb4ODm0Zarf1kS5BM=
-github.com/klauspost/cpuid/v2 v2.2.8/go.mod h1:Lcz8mBdAVJIBVzewtcLocK12l3Y+JytZYpaMropDUws=
 github.com/konsorten/go-windows-terminal-sequences v1.0.1/go.mod h1:T0+1ngSBFLxvqU3pZ+m/2kptfBszLMUkC4ZK/EgS/cQ=
 github.com/konsorten/go-windows-terminal-sequences v1.0.3/go.mod h1:T0+1ngSBFLxvqU3pZ+m/2kptfBszLMUkC4ZK/EgS/cQ=
 github.com/kr/logfmt v0.0.0-20140226030751-b84e30acd515/go.mod h1:+0opPa2QZZtGFBFZlji/RkVcI2GknAs/DXo4wKdlNEc=
@@ -338,14 +331,10 @@ github.com/matttproud/golang_protobuf_extensions v1.0.1/go.mod h1:D8He9yQNgCq6Z5
 github.com/mediocregopher/radix/v3 v3.4.2/go.mod h1:8FL3F6UQRXHXIBSPUs5h0RybMF8i4n7wVopoX3x7Bv8=
 github.com/microcosm-cc/bluemonday v1.0.2/go.mod h1:iVP4YcDBq+n/5fb23BhYFvIMq/leAFZyRl6bYmGDlGc=
 github.com/miekg/dns v1.0.14/go.mod h1:W1PPwlIAgtquWBMBEV9nkV9Cazfe8ScdGz/Lj7v3Nrg=
-github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260202035249-b2c299650f7e h1:PAyiC+wYbWnuP1sRhpIeaG3EOEZoeRA1lE6x6jWcmX8=
-github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260202035249-b2c299650f7e/go.mod h1:/6UT4zZl6awVeXLeE7UGDWZvXj3IWkRsh3mqsn0DiAs=
+github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260304062516-e7e54d966ef2 h1:OEqYnraL/Tf2nGMjRJji6xaaDe40zSQVBvruzEBZ1hU=
+github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260304062516-e7e54d966ef2/go.mod h1:/6UT4zZl6awVeXLeE7UGDWZvXj3IWkRsh3mqsn0DiAs=
 github.com/milvus-io/milvus/pkg/v2 v2.6.4-0.20251104142533-a2ce70d25256 h1:M2waty0w2k4YT2HHzJk3fx6EFPD4DKxNJatitIV+gGU=
 github.com/milvus-io/milvus/pkg/v2 v2.6.4-0.20251104142533-a2ce70d25256/go.mod h1:HT6Wxahwj/l8+i+D/C3iwDzCjDa36U9gyVw6CjjK4pE=
-github.com/minio/md5-simd v1.1.2 h1:Gdi1DZK69+ZVMoNHRXJyNcxrMA4dSxoYHZSQbirFg34=
-github.com/minio/md5-simd v1.1.2/go.mod h1:MzdKDxYpY2BT9XQFocsiZf/NKVtR7nkE4RoEpN+20RM=
-github.com/minio/minio-go/v7 v7.0.73 h1:qr2vi96Qm7kZ4v7LLebjte+MQh621fFWnv93p12htEo=
-github.com/minio/minio-go/v7 v7.0.73/go.mod h1:qydcVzV8Hqtj1VtEocfxbmVFa2siu6HGa+LDEPogjD8=
 github.com/mitchellh/cli v1.0.0/go.mod h1:hNIlj7HEI86fIcpObd7a0FcrxTWetlwJDGcceTlRvqc=
 github.com/mitchellh/go-homedir v1.0.0/go.mod h1:SfyaCUpYCn1Vlf4IUYiD9fPX4A5wJrkLzIz1N1q0pr0=
 github.com/mitchellh/go-homedir v1.1.0/go.mod h1:SfyaCUpYCn1Vlf4IUYiD9fPX4A5wJrkLzIz1N1q0pr0=
@@ -437,8 +426,6 @@ github.com/rogpeppe/go-internal v1.8.1/go.mod h1:JeRgkft04UBgHMgCIwADu4Pn6Mtm5d4
 github.com/rogpeppe/go-internal v1.9.0/go.mod h1:WtVeX8xhTBvf0smdhujwtBcq4Qrzq/fJaraNFVN+nFs=
 github.com/rogpeppe/go-internal v1.14.1 h1:UQB4HGPB6osV0SQTLymcB4TgvyWu6ZyliaW0tI/otEQ=
 github.com/rogpeppe/go-internal v1.14.1/go.mod h1:MaRKkUm5W0goXpeCfT7UZI6fk/L7L7so1lCWt35ZSgc=
-github.com/rs/xid v1.5.0 h1:mKX4bl4iPYJtEIxp6CYiUuLQ/8DYMoz0PUdtGgMFRVc=
-github.com/rs/xid v1.5.0/go.mod h1:trrq9SKmegXys3aeAKXMUTdJsYXVwGY3RLcfgqegfbg=
 github.com/russross/blackfriday v1.5.2/go.mod h1:JO/DiYxRf+HjHt06OyowR9PTA263kcR/rfWxYHBV53g=
 github.com/russross/blackfriday/v2 v2.0.1/go.mod h1:+Rmxgy9KzJVeS9/2gXHxylqXiyQDYRxCVz55jmeOWTM=
 github.com/ryanuber/columnize v0.0.0-20160712163229-9b3edd62028f/go.mod h1:sm1tb6uqfes/u+d4ooFouqFdy9/2g9QGwK3SQygK0Ts=
@@ -736,7 +723,6 @@ golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6/go.mod h1:oPkhp1MJrh7nUepCBc
 golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.0.0-20220209214540-3681064d5158/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
-golang.org/x/sys v0.5.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.8.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.11.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.15.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=

go.mod
@@ -19,7 +19,7 @@ require (
 	github.com/google/btree v1.1.2
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/klauspost/compress v1.18.0
-	github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260202035249-b2c299650f7e
+	github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260304062516-e7e54d966ef2
 	github.com/minio/minio-go/v7 v7.0.73
 	github.com/panjf2000/ants/v2 v2.11.3 // indirect
 	github.com/pingcap/log v1.1.1-0.20221015072633-39906604fb81 // indirect

go.sum
@@ -797,8 +797,8 @@ github.com/milvus-io/cgosymbolizer v0.0.0-20250318084424-114f4050c3a6 h1:YHMFI6L
 github.com/milvus-io/cgosymbolizer v0.0.0-20250318084424-114f4050c3a6/go.mod h1:DvXTE/K/RtHehxU8/GtDs4vFtfw64jJ3PaCnFri8CRg=
 github.com/milvus-io/gorocksdb v0.0.0-20220624081344-8c5f4212846b h1:TfeY0NxYxZzUfIfYe5qYDBzt4ZYRqzUjTR6CvUzjat8=
 github.com/milvus-io/gorocksdb v0.0.0-20220624081344-8c5f4212846b/go.mod h1:iwW+9cWfIzzDseEBCCeDSN5SD16Tidvy8cwQ7ZY8Qj4=
-github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260202035249-b2c299650f7e h1:PAyiC+wYbWnuP1sRhpIeaG3EOEZoeRA1lE6x6jWcmX8=
-github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260202035249-b2c299650f7e/go.mod h1:/6UT4zZl6awVeXLeE7UGDWZvXj3IWkRsh3mqsn0DiAs=
+github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260304062516-e7e54d966ef2 h1:OEqYnraL/Tf2nGMjRJji6xaaDe40zSQVBvruzEBZ1hU=
+github.com/milvus-io/milvus-proto/go-api/v2 v2.6.6-0.20260304062516-e7e54d966ef2/go.mod h1:/6UT4zZl6awVeXLeE7UGDWZvXj3IWkRsh3mqsn0DiAs=
 github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8 h1:AMFGa4R4MiIpspGNG7Z948v4n35fFGB3RR3G/ry4FWs=
 github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8/go.mod h1:mC1jAcsrzbxHt8iiaC+zU4b1ylILSosueou12R++wfY=
 github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3 h1:+n/aFZefKZp7spd8DFdX7uMikMLXX4oubIzJF4kv/wI=

internal/core/src/common/QueryResult.h
@@ -368,6 +368,13 @@ struct RetrieveResult {
     bool has_more_result = true;
     // record the storage usage in retrieve
     StorageCost retrieve_storage_cost_;
+
+    // Element-level query support
+    // When element_level_ is true:
+    //   - result_offsets_ contains unique doc_ids (no duplicates)
+    //   - element_indices_[i] contains all matching element indices for result_offsets_[i]
+    bool element_level_{false};
+    std::vector<std::vector<int32_t>> element_indices_;
 };
 
 using RetrieveResultPtr = std::shared_ptr<RetrieveResult>;

internal/core/src/exec/operator/ElementFilterNode.cpp
@@ -15,6 +15,7 @@
 // limitations under the License.
 
 #include "ElementFilterNode.h"
+#include "exec/operator/Utils.h"
 
 #include <algorithm>
 #include <chrono>
@@ -51,7 +52,8 @@ PhyElementFilterNode::PhyElementFilterNode(
                operator_id,
                element_filter_node->id(),
                "PhyElementFilterNode"),
-      struct_name_(element_filter_node->struct_name()) {
+      struct_name_(element_filter_node->struct_name()),
+      has_doc_predicate_(element_filter_node->has_doc_predicate()) {
     ExecContext* exec_context = operator_context_->get_exec_context();
     query_context_ = exec_context->get_query_context();
     std::vector<expr::TypedExprPtr> exprs;
@@ -125,9 +127,19 @@ PhyElementFilterNode::GetOutput() {
 
     // Step 3: Update search result with wrapped iterators
     search_result.vector_iterators_ = std::move(wrapped_iterators);
+    size_t num_iterators = search_result.vector_iterators_.has_value()
+                               ? search_result.vector_iterators_->size()
+                               : 0;
+
+    // Step 4: If no doc-level predicate, collect results directly
+    // (otherwise, downstream FilterNode will do this)
+    if (!has_doc_predicate_) {
+        CollectResults(search_result, array_offsets.get());
+    }
+
     query_context_->set_search_result(std::move(search_result));
 
-    // Step 4: Record metrics
+    // Step 5: Record metrics
     std::chrono::high_resolution_clock::time_point end_time =
         std::chrono::high_resolution_clock::now();
     double cost =
@@ -136,13 +148,90 @@ PhyElementFilterNode::GetOutput() {
 
     tracer::AddEvent(
         fmt::format("PhyElementFilterNode: wrapped {} iterators, struct_name: "
-                    "{}, cost_us: {}",
-                    wrapped_iterators.size(),
+                    "{}, has_doc_predicate: {}, cost_us: {}",
+                    num_iterators,
                     struct_name_,
+                    has_doc_predicate_,
                     cost));
 
     // Pass through input to downstream
     return input_;
+}
+
+void
+PhyElementFilterNode::CollectResults(SearchResult& search_result,
+                                     const IArrayOffsets* array_offsets) {
+    // When there's no doc-level predicate, we need to consume the iterators
+    // and collect the top-K results ourselves.
+    //
+    // Note: knowhere iterator doesn't guarantee strictly ordered output,
+    // so we must use binary insertion to maintain sorted order.
+
+    auto& iterators = search_result.vector_iterators_.value();
+    int64_t nq = search_result.total_nq_;
+    int64_t topk = search_result.unity_topK_;
+
+    knowhere::MetricType metric_type = query_context_->get_metric_type();
+    bool large_is_better = PositivelyRelated(metric_type);
+
+    // Initialize result arrays
+    search_result.seg_offsets_.resize(nq * topk, INVALID_SEG_OFFSET);
+    search_result.distances_.resize(nq * topk, 0.0f);
+    search_result.element_indices_.resize(nq * topk, -1);
+
+    for (int64_t q = 0; q < nq; ++q) {
+        auto& iterator = iterators[q];
+        int64_t count = 0;
+        int64_t base_idx = q * topk;
+
+        while (iterator->HasNext() && count < topk) {
+            auto result = iterator->Next();
+            if (!result.has_value()) {
+                break;
+            }
+
+            auto [element_id, distance] = result.value();
+            auto [doc_id, elem_idx] =
+                array_offsets->ElementIDToRowID(element_id);
+
+            // Find insert position using binary search
+            size_t pos =
+                large_is_better
+                    ? find_binsert_position<true>(search_result.distances_,
+                                                  base_idx,
+                                                  base_idx + count,
+                                                  distance)
+                    : find_binsert_position<false>(search_result.distances_,
+                                                   base_idx,
+                                                   base_idx + count,
+                                                   distance);
+
+            // Shift elements to make room for insertion
+            if (count > 0 && pos < base_idx + count) {
+                std::memmove(&search_result.distances_[pos + 1],
+                             &search_result.distances_[pos],
+                             (base_idx + count - pos) * sizeof(float));
+                std::memmove(&search_result.seg_offsets_[pos + 1],
+                             &search_result.seg_offsets_[pos],
+                             (base_idx + count - pos) * sizeof(int64_t));
+                std::memmove(&search_result.element_indices_[pos + 1],
+                             &search_result.element_indices_[pos],
+                             (base_idx + count - pos) * sizeof(int32_t));
+            }
+
+            // Insert the new result
+            search_result.seg_offsets_[pos] = doc_id;
+            search_result.element_indices_[pos] = elem_idx;
+            search_result.distances_[pos] = distance;
+            ++count;
+        }
+    }
+
+    // Clear iterators to indicate results have been collected
+    search_result.vector_iterators_.reset();
+
+    tracer::AddEvent(fmt::format(
+        "PhyElementFilterNode::CollectResults: nq={}, topk={}", nq, topk));
 }
 
 }  // namespace exec

internal/core/src/exec/operator/ElementFilterNode.h
@@ -79,9 +79,14 @@ class PhyElementFilterNode : public Operator {
     }
 
  private:
+    void
+    CollectResults(SearchResult& search_result,
+                   const IArrayOffsets* array_offsets);
+
     std::unique_ptr<ExprSet> element_exprs_;
     QueryContext* query_context_;
     std::string struct_name_;
+    bool has_doc_predicate_{true};
     bool is_finished_{false};
 };
 

commit 4e98954cc57d0f9bd3ff2988db3866e0436f7e8a

fix: fix compilation error in ExecPlanNodeVisitor for element-level query (#48254)
## Summary

- Fix master branch compilation error introduced by PR #47906
- `QueryContext::element_level_query()` does not exist, replaced with
`bitset_is_element_level()` which is the correct method defined in
`QueryContext.h`

## Test plan

- [ ] CI build passes (currently master is broken)
- [ ] Existing StructArray e2e tests pass

issue: #48253

🤖 Generated with [Claude Code](https://claude.com/claude-code)

Signed-off-by: xiaofanluan <xiaofan.luan@zilliz.com>
Co-authored-by: Claude Opus 4.6 <noreply@anthropic.com>

internal/core/src/query/ExecPlanNodeVisitor.cpp
@@ -338,7 +338,7 @@ ExecPlanNodeVisitor::setupRetrieveResult(
     tmp_retrieve_result.total_data_cnt_ = first_column->size();
     if (first_column->IsBitmap()) {
         BitsetTypeView view(first_column->GetRawData(), first_column->size());
-        if (query_context->element_level_query()) {
+        if (query_context->bitset_is_element_level()) {
             // Element-level query: bitset is element-level, need to convert to (doc_id, element_index)
             tmp_retrieve_result.element_level_ = true;
             tracer::AutoSpan _(